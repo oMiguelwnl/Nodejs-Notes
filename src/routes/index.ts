c42ef19c9709a37185677032fdef1aa456a8d1ab
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-03-12 15:45:51 -0300

    Aprendendo a usar Mustache

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,13 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("Olá Mundo");
+  let age: number = 15;
+  let showOld: boolean = false;
+
+  if (age > 50) {
+    showOld = true;
+  }
+  res.render("home", { name: "Friend", showWelcome: true, age: 90, showOld });
 });
 router.get("/contato", (req: Request, res: Response) => {
   res.send("Página de contato");
