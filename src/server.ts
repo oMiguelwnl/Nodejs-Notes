c9bb7f3506e57da7aedeb79d1042c62033086511
oMiguelwnl <contato.Miguel.almeida7@gmail.com>
2024-03-12 15:08:42 -0300

    Criando uma página 404 e aprendendo sobre pasta pública e arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import path from "path";
 import mainRoutes from "./routes/index";
-import painelRoutes from "./routes/painel";
 
 const server = express();
 
+server.use(express.static(path.join(__dirname, "../public")));
+
 server.use(mainRoutes);
-server.use("/painel", painelRoutes);
+
+server.use((req: Request, res: Response) => {
+  res.status(404).send("Not found");
+});
 
 server.listen(3333);
